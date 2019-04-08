0df310dbe38604a390dea7ae0b0cfae68fbc0e99
Try Ajitiono <ballinst@gmail.com>
2019-04-08 15:51:53 +1000

    Add try catch
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- services/frontend/src/Main.js
+++ services/frontend/src/Main.js
@@ -1,20 +1,36 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Container from './ListProfiles';
 import Detail from './Detail';
 import { MembersContext } from './contexts/MembersProvider';
+import { withStyles } from '@material-ui/core/styles';
 
 let interval;
 
-function Main() {
+const styles = {
+  errorText: {
+    color: 'red',
+    paddingLeft: 24
+  }
+};
+
+function Main({ classes }) {
   const { members, setMembers } = useContext(MembersContext);
+  const [error, setError] = useState(undefined);
 
   useEffect(() => {
     if (interval === undefined) {
       interval = setInterval(async () => {
-        const response = await fetch('/v1/users');
-        setMembers(await response.json());
+        setError(undefined);
+
+        try {
+          const response = await fetch('/v1/users');
+
+          setMembers(await response.json());
+        } catch (err) {
+          setError(err.message);
+        }
         // This is for mock during development.
         // const changed = Math.floor(Math.random() * 5);
         // const newV = { ...members[changed] };
@@ -32,18 +48,21 @@ function Main() {
   }, []);
 
   return (
-    <Switch>
-      <Route
-        path="/"
-        exact
-        render={props => <Container {...props} members={members} />}
-      />
-      <Route
-        path="/:id"
-        render={props => <Detail {...props} members={members} />}
-      />
-    </Switch>
+    <div>
+      {error && <h5 className={classes.errorText}>{error}</h5>}
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={props => <Container {...props} members={members} />}
+        />
+        <Route
+          path="/:id"
+          render={props => <Detail {...props} members={members} />}
+        />
+      </Switch>
+    </div>
   );
 }
 
-export default Main;
+export default withStyles(styles)(Main);
